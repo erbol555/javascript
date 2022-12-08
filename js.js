6a236f5052d278f89939b30ed954a493bd8deb80
baktiaruuluerbol5@gmail.com <bantiaruuluerbol5@gmail.com>
2022-12-09 03:17:19 +0600

    first

---
+++ js.js
@@ -0,0 +1,17 @@
+let a = 7;
+let b = 2;
+console.log(7 % 2);
+
+console.log(true * false);
+alert(str);
+var str = "hello";
+let arr = [1, 2, 4, 4];
+arr.push(2);
+console.log(arr);
+console.log(Boolean(0));
+const first = 2;
+
+console.log(first);
+console.log("2" * "3");
+let num = parseFloat("12.5px");
+console.log(num);
